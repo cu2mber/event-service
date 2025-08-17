19f8be2efbbe1771fcb7f6fe3fd3593ab1ea79ac
rudduddl <kim5472678@gmail.com>
2025-08-17 17:18:13 +0900

    h2 데이터베이스 연동 및 크롤링 코드 작업 진행 중

--- src/main/java/com/example/eventservice/crawler/event_crawler.py
+++ src/main/java/com/example/eventservice/crawler/event_crawler.py
@@ -1,8 +1,10 @@
-import re
+import requests as req
 import json
-import os
 from datetime import datetime
 from ..repository.crawler.db_connection import DBManager
+from dotenv import load_dotenv
+import os
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -11,9 +13,27 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
+load_dotenv(".env")
+
+event_url = os.getenv("EVENT_URL")
 
 class EventCrawler:
     def __init__(self, debug=False, startPage=1):
+        # 데이터베이스 불러오는 함수
         self.db = DBManager()
+
+        driver = webdriver.Chrome()
+        driver.get(event_url)
+
+        # 페이지가 완전히 로딩되도록 3초동안 기다림
+        time.sleep(3)
+
+        # 목록에 있는 축제 클릭
+        start_event = driver.find_element(By.CLASS_NAME, ".go")
+        start_event.click()
+
+
+
+
+
--- src/main/java/com/example/eventservice/repository/crawler/db_connection.py
+++ src/main/java/com/example/eventservice/repository/crawler/db_connection.py
@@ -1,1 +1,24 @@
-def DBManager:
+from dotenv import load_dotenv
+import os
+import jaydebeapi
+
+load_dotenv(".env")
+
+db_url = os.getenv("DB_URL")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_driver = os.getenv("DB_DRIVER")
+db_jar = os.getenv("DB_JAR")
+class DBManager:
+    def __init__(self):
+        conn = jaydebeapi.connect(
+            db_driver,
+            db_url,
+            [db_user, db_password],
+            db_jar
+        )
+
+        cursor = conn.cursor()
+        # cursor.execute("SELECT * FROM events")
+        result = cursor.fetchall()
+
